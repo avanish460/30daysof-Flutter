5564c5104556b13ddc3e37d93a42f01cd45f0b1f
avanish460 <avanish460@gmail.com>
2023-05-17 00:43:04 +0530

    appBarTheme, extracting themes

---
+++ lib/widgets/themes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTheme {
+  static ThemeData get lightTheme => ThemeData(
+      primarySwatch: Colors.amber,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      appBarTheme: AppBarTheme(
+        color: Colors.white,
+        elevation: 0.0,
+      ));
+
+  static ThemeData get darkTheme => ThemeData(
+        brightness: Brightness.dark,
+      );
+}
