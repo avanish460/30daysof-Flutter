5d6a703ba3b4873b6d66c5291118cef1454a3757
avanish460 <avanish460@gmail.com>
2023-05-17 20:13:22 +0530

    gridview,gridDelegate,crossAxisCount,Spacing_main&crossAxis,GridTile

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -42,15 +42,56 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items.isNotEmpty)? ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: CatalogModel.items[index],
-            );
-          },
-        ):Center(child: CircularProgressIndicator(),
-        ),
+        child: (CatalogModel.items.isNotEmpty)
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                        header: Container(
+                          child: Text(item.name),
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.amber,
+                          ),
+                        ),
+                        child: Image.network(
+                          item.image,
+                        ),
+                        footer: Container(
+                          padding: EdgeInsets.all(12),
+                          child: Text(
+                            item.price.toString(),
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ));
+                },
+                itemCount: CatalogModel.items.length,
+              )
+            // ListView.builder(
+            //   itemCount: CatalogModel.items.length,
+            //   itemBuilder: (context, index) {
+            //     return ItemWidget(
+            //       item: CatalogModel.items[index],
+            //     );
+            //   },
+            // )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
